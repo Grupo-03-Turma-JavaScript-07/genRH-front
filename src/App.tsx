d408b69938d2d5b0b1a61156d8930cf2ee0ca3d5
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-08-08 10:17:05 -0300

    Estrutura inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/home/Home'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Login from './pages/login/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+            <Navbar />
+            <div className='min-h-[80vh]'>
+              <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/home' element={<Home />} />
+                {/* <Route path='/' element={< />} /> */}
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+    </>
+  )
+}
+
+export default App
