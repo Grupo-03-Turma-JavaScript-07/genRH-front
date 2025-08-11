fa856351479523802a2523c3c13693d5737b645c
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-08-11 13:49:23 -0300

    style: Alterar apresentação dos cards de departamentos para o formato de lista

--- src/components/departamentos/listadepartamento/Listadepartamento.tsx
+++ src/components/departamentos/listadepartamento/Listadepartamento.tsx
@@ -19,14 +19,26 @@ function ListaDepartamento() {
   return (
     <div className="flex justify-center w-full my-4">
       <div className="container flex flex-col">
+        <h1 className="text-3xl text-center font-semibold p-2 my-4">Departamentos</h1>
         {departamentos.length === 0 ? (
           <p className="text-center text-xl">Carregando departamentos...</p>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {departamentos.map((departamento) => (
-              <CartDepartamento key={departamento.id} departamento={departamento} />
-            ))}
-          </div>
+          <>
+            <div className="w-[70vw] mx-auto">
+              <table className="w-full text-sm text-left rtl:text-right border overflow-hidden text-gray-500 dark:text-gray-400">
+                <thead className="text-md text-white uppercase bg-gray-50 dark:bg-slate-700">
+                  <tr className="bg-white border-b dark:bg-gray-500 dark:border-gray-700 text-center text-lg border-gray-200">
+                    <th scope="col" className="px-6 py-3">Departamento</th>
+                    <th scope="col" className="px-6 py-3"></th>
+                    <th scope="col" className="px-6 py-3"></th>
+                  </tr>
+                </thead>
+                {departamentos.map((departamento) => (
+                  <CartDepartamento key={departamento.id} departamento={departamento} />
+                ))}
+              </table>
+            </div>
+          </>
         )}
       </div>
     </div>
